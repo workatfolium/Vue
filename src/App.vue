7f7dfde4dfc027c2293bf752b1135aa639bc5485
workatfolium <info.work.iamjayrajsinhsolanki@gmail.com>
2022-06-07 12:04:34 +0530

    component created!

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <HelloWorld msg="Hello World!"></HelloWorld>
+  <img alt="vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Hello World Component!"></HelloWorld>
+  <HomeComponent />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/HomeComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomeComponent
   }
 }
 </script>
+
+<style>
+#app{
+  text-align: center;
+}
+</style>
 
